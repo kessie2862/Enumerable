f39f44ab332957dab6007d5ac180b96b09bbe150
ANTOINE1128 <makdessyantoine@gmail.com>
2023-07-27 16:44:32 +0300

    add methods

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,26 +1,24 @@
-# module MyEnumerable
-#   # Checks if all elements in the collection satisfy the given condition.
-#   def all?(&block)
-#     each do |item|
-#       return false unless block.call(item)
-#     end
-#     true
-#   end
+module MyEnumerable
 
-#   # Checks if any element in the collection satisfies the given condition.
-#   def any?(&block)
-#     each do |item|
-#       return true if block.call(item)
-#     end
-#     false
-#   end
+  def all?(&block)
+    each do |item|
+      return false unless block.call(item)
+    end
+    true
+  end
 
-#   # Filters the collection, keeping only elements that satisfy the given condition.
-#   def filter(&block)
-#     result = []
-#     each do |item|
-#       result << item if block.call(item)
-#     end
-#     result
-#   end
-# end
+  def any?(&block)
+    each do |item|
+      return true if block.call(item)
+    end
+    false
+  end
+
+  def filter(&block)
+    result = []
+    each do |item|
+      result << item if block.call(item)
+    end
+    result
+  end
+end
